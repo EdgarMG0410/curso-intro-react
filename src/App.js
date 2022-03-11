42e87f95b365b9a9b5202d05e9631faedea1c59b
EdgarMG0410 <emancillas04@gmail.com>
2022-03-10 21:33:20 -0600

    Repositorio creado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {TodoCounter} from './TodoCounter';
+
+const todos =[
+  { text: 'Cortar cebolla', completed: false},
+  { text: 'Tomar curso de intro a React', completed: false},
+  { text: 'Llorar con la llorona', completed: false}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+    
+
+    {/* <TodoSearch /> */}
+    <input placeholder='wait...'/>
+  {/*   <TodoList>
+      {todos.map(todo => ()
+        <TodoItem/>
+      )}
+    </TodoList> */}
+  {/* <CreateTodoButton/> */}
+  <button>+</button>
+    
+    </React.Fragment>
   );
 }
 
