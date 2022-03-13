fda40ff20ca43f56b330a603c3d714444d2735da
EdgarMG0410 <emancillas04@gmail.com>
2022-03-12 21:15:21 -0600

    Uso de eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,10 +1,20 @@
 import React from "react";
 import './CreateTodoButton.css';
 
-function CreateTodoButton() {
-    return (
-      <button className="CreateTodoButton">+</button>
-    );
+
+function CreateTodoButton(props) {
+  const onClickButton = (msg) => {
+    alert(msg);
+  };
+
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={() => onClickButton('Aqui va el modal')}
+      >
+      +
+    </button>
+  );
 }
 
 export {CreateTodoButton};
